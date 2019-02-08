e2758581039353a3bb377a5a2e1272d30e834fe4
arsley <blue20will@gmail.com>
2019-02-08 14:14:55 +0900

    add: Create generator with templates

---
+++ lib/generators/rails/stimulus_generator.rb
@@ -0,0 +1,37 @@
+require 'rails/generators/named_base'
+
+class Rails::Generators::StimulusGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('templates', __dir__)
+
+  argument :actions, type: :array, default: [], banner: 'action action'
+
+  def create_controller_dir
+    empty_directory(controller_path) unless File.directory?(controller_path)
+  end
+
+  def copy_controller_file
+    actions.each do |action|
+      @action = action
+      template('controller.js', controller_path + "#{action}_controller.js")
+    end
+  end
+
+  def copy_stylesheet_file
+    template('controller.scss', stylesheet_path + "#{file_name}.scss")
+  end
+
+  private
+
+  def source_path
+    ::Webpacker.instance.config.source_path
+  end
+
+  def controller_path
+    source_path + "javascripts/controllers/#{file_name}"
+  end
+
+  def stylesheet_path
+    source_path + 'stylesheets'
+  end
+end
+
